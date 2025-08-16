410381443f7d1eb540fb79fedf1b23f5053ae517
sbx_user1051 <sbx_user1051@169.254.36.143>
2025-08-16 09:42:25 +0000

    Sat Aug 16 09:42:25 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -18,20 +18,27 @@ class CheckoutSolution:
     def checkout(self, skus):
         
         skus = skus.upper()
-        item_freq = Counter(skus)
+        all_items_freq = Counter(skus)
 
-        if any(x not in self.prices.keys() for x in item_freq.keys()):
+        if any(x not in self.prices.keys() for x in all_items_freq.keys()):
             return -1
 
-        for item in item_freq.keys():
-            pass
+        for item in self.offers.keys():
+
+            item_num = 
+
+            if all_items_freq[item] >= (self.offers[item])[0]:
+
+                num_offers = all_items_freq[item] // (self.offers[item])[0]
+                all_items_freq[item] = all_items_freq[item] % (self.offers[item])[0]
 
 
-        items_cost = {k: item_freq[k] * self.prices[k] for k in item_freq.keys()}
+        items_cost = {k: all_items_freq[k] * self.prices[k] for k in all_items_freq.keys()}
         total = sum(items_cost.values())
 
         return total
         
+
 
 
 
